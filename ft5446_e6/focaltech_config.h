2712a12ac22956d2600eaeed7a17948817d08ea3
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2024-09-08 08:18:51 +0000

    msm8953: ft5446: drop all fts chip type "except ft5446"
    Signed-off-by: RenzAlt <RenzAlt@gmail.com>

--- ft5446_e6/focaltech_config.h
+++ ft5446_e6/focaltech_config.h
@@ -34,51 +34,7 @@
 /**************************************************/
 /****** G: A, I: B, S: C, U: D  ******************/
 /****** chip type defines, do not modify *********/
-#define _FT8716     0x87160805
-#define _FT8736     0x87360806
-#define _FT8006     0x80060807
-#define _FT8606     0x86060808
-#define _FT8607     0x86070809
-#define _FTE716     0xE716080a
-
-#define _FT5416     0x54160002
-#define _FT5426     0x54260002
-#define _FT5435     0x54350002
-#define _FT5436     0x54360002
-#define _FT5526     0x55260002
-#define _FT5526I    0x5526B002
 #define _FT5446     0x54460002
-#define _FT5346     0x53460002
-#define _FT5446I    0x5446B002
-#define _FT5346I    0x5346B002
-#define _FT7661     0x76610002
-#define _FT7511     0x75110002
-#define _FT7421     0x74210002
-#define _FT7681     0x76810002
-#define _FT3C47U    0x3C47D002
-#define _FT3417     0x34170002
-#define _FT3517     0x35170002
-#define _FT3327     0x33270002
-#define _FT3427     0x34270002
-
-#define _FT5626     0x56260001
-#define _FT5726     0x57260001
-#define _FT5826B    0x5826B001
-#define _FT5826S    0x5826C001
-#define _FT7811     0x78110001
-#define _FT3D47     0x3D470001
-#define _FT3617     0x36170001
-#define _FT3717     0x37170001
-#define _FT3817B    0x3817B001
-
-#define _FT6236U    0x6236D003
-#define _FT6336G    0x6336A003
-#define _FT6336U    0x6336D003
-#define _FT6436U    0x6436D003
-
-#define _FT3267     0x32670004
-#define _FT3367     0x33670004
-
 
 
 /*************************************************/
